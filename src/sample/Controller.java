aaae0f1a59601c537bc4c205b7e3bb4b46cc690d
strahsvid <strahsvid@protonmail.com>
2021-04-22 02:59:15 +0300

    Initialized all fields in Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,30 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+
+    @FXML public ImageView playButton;
+    @FXML public ImageView nextButton;
+    @FXML public ImageView backButton;
+    @FXML public ImageView songImage;
+    @FXML public ImageView volumeButton;
+
+    @FXML public Label songName;
+    @FXML public Label timerMusic;
+    @FXML public Label timeOfSong;
+
+    @FXML public Slider musicSlider;
+    @FXML public Slider volumeSlider;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
